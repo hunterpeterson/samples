bfb31f86e62c74ab8d7b6f070a130ee48e5a2a65
hpeterso <hpeterso@adobe.com>
2016-05-23 16:59:42 -0600

    clean up

--- SampleLocalNotification/SampleLocalNotification/AppDelegate.m
+++ SampleLocalNotification/SampleLocalNotification/AppDelegate.m
@@ -51,9 +51,4 @@
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:notification.userInfo[@"adb_deeplink"]]];
     }
 }
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *)options {
-    
-    return YES;
-}
 @end
